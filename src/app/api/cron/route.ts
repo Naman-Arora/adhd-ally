23e7a943d8805f1cd519bf2c8a86a33735228fed
Naman Arora <namanarora166@gmail.com>
2024-05-10 17:30:39 -0500

    added cron job api route

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,30 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { pool } from "@/lib/db";
+
+export async function GET(req: NextRequest) {
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json({ message: "Unauthorized!" }, { status: 401 });
+  }
+  try {
+    const { rows: states } = await pool.query(
+      "SELECT * from states ORDER BY name"
+    );
+
+    const { rows: professionals } = await pool.query(
+      "SELECT * from professionals ORDER BY name"
+    );
+
+    const { rows: supportgroups } = await pool.query(
+      "SELECT * from groups ORDER BY name"
+    );
+
+    return NextResponse.json({ states, professionals, supportgroups });
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Falied to connect to database." },
+      { status: 500 }
+    );
+  }
+}
